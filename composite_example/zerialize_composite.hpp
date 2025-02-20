ad3b64551a18919506333fa8739973c62731c3f4
Colin Prepscius <colinprepscius@gmail.com>
2025-02-20 09:04:33 -0500

    composite cleanup

--- composite_example/zerialize_composite.hpp
+++ composite_example/zerialize_composite.hpp
@@ -19,39 +19,6 @@ public:
     std::string to_string() const {
         return "<MyComposite a: " + std::to_string(a) + " b: " + b + " />";
     }
-
-    // std::map<std::string, std::any> serialize() const {
-    //     return std::map<std::string, std::any> {
-    //         {"s", (uint64_t)D}, 
-    //         {"a", a}, 
-    //         {"b", b}
-    //     };
-    // }
-
-    // zerialize::ZComposite<MyComposite> serializer() const { 
-    //     return zerialize::ZComposite<MyComposite>(*this); 
-    // }
-
-    // template<typename SerializerType>
-    // void serialize_write(SerializerType& serializer) {
-    //     return serializer.serialize({
-    //         {"s", (uint64_t)D}, 
-    //         {"a", a}, 
-    //         {"b", b}
-    //     });
-    // }
-
-    // template<typename SerializerType>
-    // typename SerializerType::BufferType serialize(SerializerType& serializer) {
-    //     if constexpr (DEBUG_TRACE_CALLS) {
-    //         cout << "MyComposite::serialize" << endl;
-    //     }
-    //     return serializer.serialize({
-    //         {"s", (uint64_t)D}, 
-    //         {"a", a}, 
-    //         {"b", b}
-    //     });
-    // }
 };
 
 template <typename T, size_t D>
@@ -63,22 +30,22 @@ map<string, any> serialize(const MyComposite<T, D>& c) {
     };
 }
 
-template<typename SerializerType, typename T, size_t D>
-typename SerializerType::BufferType serialize(SerializerType& serializer, const MyComposite<T, D>& c) {
-    if constexpr (DEBUG_TRACE_CALLS) {
-        cout << "serialize(Composite&) up" << endl;
-    }
-    return serializer.serialize(serialize<T, D>(c));
-}
+// template<typename SerializerType, typename T, size_t D>
+// typename SerializerType::BufferType serialize(SerializerType& serializer, const MyComposite<T, D>& c) {
+//     if constexpr (DEBUG_TRACE_CALLS) {
+//         cout << "serialize(Composite&) up" << endl;
+//     }
+//     return serializer.serialize(serialize<T, D>(c));
+// }
 
-template<typename SerializerType, typename T, size_t D>
-typename SerializerType::BufferType serialize(const MyComposite<T, D>& c) {
-    if constexpr (DEBUG_TRACE_CALLS) {
-        cout << "serialize(Composite&)" << endl;
-    }   
-    SerializerType serializer;
-    return serialize(serializer, c);
-}
+// template<typename SerializerType, typename T, size_t D>
+// typename SerializerType::BufferType serialize(const MyComposite<T, D>& c) {
+//     if constexpr (DEBUG_TRACE_CALLS) {
+//         cout << "serialize(Composite&)" << endl;
+//     }   
+//     SerializerType serializer;
+//     return serialize(serializer, c);
+// }
 
 inline bool _vectorContains(const vector<string_view>& v, const string_view& c) {
     return std::ranges::find(v, c) != v.end();
